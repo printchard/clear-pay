fb146680812c9fb45d28658f635cf7816f33f992
Ricardo Fernández <ricardo.a.fernandez.a@gmail.com>
2025-03-07 23:23:17 -0600

    db: update seed command

--- app/db/seed.ts
+++ app/db/seed.ts
@@ -1,25 +1,54 @@
 import { db } from "./db";
-import { contacts, users } from "./schema";
+import { contacts, debts, users } from "./schema";
+
+const userId = crypto.randomUUID();
+const contactId = crypto.randomUUID();
+
+async function seedUsers() {
+  await db
+    .insert(users)
+    .values({
+      id: userId,
+      name: "John Doe",
+      email: "john.doe@mail.com",
+      password: "password",
+    })
+    .execute();
+}
+
+async function seedContacts() {
+  await db
+    .insert(contacts)
+    .values({
+      firstName: "John",
+      lastName: "Doe",
+      userId,
+    })
+    .execute();
+  await db
+    .insert(contacts)
+    .values({
+      firstName: "Jane",
+      lastName: "Doe",
+      userId,
+      id: contactId,
+    })
+    .execute();
+}
+
+async function seedDebts() {
+  await db.insert(debts).values({
+    amount: 100,
+    contactId,
+    status: "pending",
+  });
+  await db.insert(debts).values({
+    amount: 200,
+    contactId,
+    status: "paid",
+  });
+}
 
 export async function seed() {
-  const userId = crypto.randomUUID();
-  Promise.all([
-    db
-      .insert(users)
-      .values({
-        id: userId,
-        name: "John Doe",
-        email: "john.doe@mail.com",
-        password: "password",
-      })
-      .execute(),
-    db
-      .insert(contacts)
-      .values({
-        firstName: "John",
-        lastName: "Doe",
-        userId,
-      })
-      .execute(),
-  ]);
+  await Promise.all([seedUsers(), seedContacts(), seedDebts()]);
 }
