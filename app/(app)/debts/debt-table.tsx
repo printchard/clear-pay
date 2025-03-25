0de25815beeed13c934d4ff4e1a7d54337fe63a7
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2025-03-24 19:13:09 -0600

    ui: make debt delete dialog functional again

--- app/(app)/debts/debt-table.tsx
+++ app/(app)/debts/debt-table.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Contact, Debt } from "@/app/db/schema";
 import { Button } from "@/components/ui/button";
 import NoItems from "@/components/ui/no-items";
@@ -15,6 +17,8 @@ import { Pencil, Trash2 } from "lucide-react";
 import Link from "next/link";
 import StatusBadge from "./status-badge";
 import DeleteDialog from "@/components/ui/delete-dialog";
+import { useState } from "react";
+import { DialogTrigger } from "@radix-ui/react-dialog";
 
 export type DebtTableProps = {
   results: {
@@ -23,61 +27,71 @@ export type DebtTableProps = {
   }[];
 };
 
-export default async function DebtTable({ results }: DebtTableProps) {
+export default function DebtTable({ results }: DebtTableProps) {
+  const [id, setId] = useState<string>("");
+
+  const action = deleteDebt.bind(null, id);
+
   if (results.length === 0) {
     return <NoItems />;
   }
 
   return (
-    <Table className="table-fixed min-w-[600px]">
-      <TableHeader>
-        <TableRow>
-          <TableHead>Contact</TableHead>
-          <TableHead>Amount</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Created At</TableHead>
-          <TableHead className="w-30"></TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {results.map(({ debt, contact }) => {
-          return (
-            <TableRow key={debt.id}>
-              <TableCell>
-                <Link
-                  href={`/contacts/${contact.id}`}
-                  className="w-full h-full block truncate"
-                >
-                  {contact.firstName} {contact.lastName}
-                </Link>
-              </TableCell>
-              <TableCell>{`$ ${debt.amount}`}</TableCell>
-              <TableCell>
-                <StatusBadge status={debt.status!} />
-              </TableCell>
-              <TableCell>
-                {dayjs(debt.createdAt).format("DD/MM/YYYY")}
-              </TableCell>
-              <TableCell className="flex justify-start gap-2">
-                <Link href={`/debts/${debt.id}/edit`}>
-                  <Button variant="secondary" size="icon">
-                    <Pencil />
-                  </Button>
-                </Link>
-                <DeleteDialog
-                  action={deleteDebt.bind(null, debt.id)}
-                  title="Are you sure you want to delete this debt?"
-                  description="This actions is irreversible"
-                >
-                  <Button variant="destructive" size="icon">
-                    <Trash2 />
-                  </Button>
-                </DeleteDialog>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <DeleteDialog
+      action={action}
+      title="Are you sure you want to delete this debt?"
+      description="This actions is irreversible"
+    >
+      <Table className="min-w-[600px] table-fixed">
+        <TableHeader>
+          <TableRow>
+            <TableHead>Contact</TableHead>
+            <TableHead>Amount</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Created At</TableHead>
+            <TableHead className="w-30"></TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {results.map(({ debt, contact }) => {
+            return (
+              <TableRow key={debt.id}>
+                <TableCell>
+                  <Link
+                    href={`/contacts/${contact.id}`}
+                    className="block h-full w-full truncate"
+                  >
+                    {contact.firstName} {contact.lastName}
+                  </Link>
+                </TableCell>
+                <TableCell>{`$ ${debt.amount}`}</TableCell>
+                <TableCell>
+                  <StatusBadge status={debt.status!} />
+                </TableCell>
+                <TableCell>
+                  {dayjs(debt.createdAt).format("DD/MM/YYYY")}
+                </TableCell>
+                <TableCell className="flex justify-start gap-2">
+                  <Link href={`/debts/${debt.id}/edit`}>
+                    <Button variant="secondary" size="icon">
+                      <Pencil />
+                    </Button>
+                  </Link>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant="destructive"
+                      size="icon"
+                      onClick={() => setId(debt.id)}
+                    >
+                      <Trash2 />
+                    </Button>
+                  </DialogTrigger>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </DeleteDialog>
   );
 }
