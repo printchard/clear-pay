f7649e03d782ee46b91609b35e2acf0f2f1187ec
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2025-03-28 12:26:45 -0600

    feat: improve ui experience

--- app/(app)/contacts/[id]/payment-info/create/page.tsx
+++ app/(app)/contacts/[id]/payment-info/create/page.tsx
@@ -1,11 +1,12 @@
 import { Card } from "@/components/ui/card";
 import { Suspense } from "react";
+import PaymentInfoFormSkeleton from "./payment-info-form-skeleton";
 import PaymentInfosForm from "./payment-infos-form";
 
 export default async function Page() {
   return (
     <Card className="p-4">
-      <Suspense>
+      <Suspense fallback={<PaymentInfoFormSkeleton />}>
         <PaymentInfosForm />
       </Suspense>
     </Card>
