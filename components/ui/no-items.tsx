3fd9d091d8a9346c8e365d9b1c1c4fa3d41f86d3
Ricardo Fernández <ricardo.a.fernandez.a@gmail.com>
2025-04-24 13:46:46 -0600

    format: format all imported components

--- components/ui/no-items.tsx
+++ components/ui/no-items.tsx
@@ -1,6 +1,6 @@
 export default function NoItems() {
   return (
-    <div className="flex flex-col items-center justify-start h-full py-20">
+    <div className="flex h-full flex-col items-center justify-start py-20">
       <p className="text-lg text-gray-500">No items found.</p>
     </div>
   );
